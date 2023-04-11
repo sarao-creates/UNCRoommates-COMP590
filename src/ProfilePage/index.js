49fb57646a000e5964db0e43dcd2fa95efd79fa5
Sameer Rao <sarao@live.unc.edu>
2023-04-10 20:30:28 -0400

    firebase

--- src/ProfilePage/index.js
+++ src/ProfilePage/index.js
@@ -1,6 +1,12 @@
+import {db} from '../Firebase/firebase.js'
+
 function ProfilePage() {
+    // const name = () => {
+    //     db.collection("Users").doc("rkEcudx9k33I5nD8TC9a").get().then((doc) => return doc.data())
+    // }
+    
     return (
-        <h1>Hi this is the profile page!</h1>
+        <h1>Hi this is the profile page! My name is Sameer</h1>
     )
 }
 
