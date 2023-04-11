afc37f7558ab66bb090dfc018d9011091561c0c3
Sameer Rao <sarao@live.unc.edu>
2023-04-10 23:59:55 -0400

    Firebase setup should be complete now

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,21 +1,24 @@
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-// import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// const config = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-//     measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-// };
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/firestore';
+// import 'firebase/compat/auth';
 
-// firebase.initializeApp(config);
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = initializeApp(config);
+
 // export const fb = firebase.auth
 
-// export const db = firebase.firestore();
-// export default db;
+export const db = getFirestore(app);
+export default db;
 
 // export const auth = firebase.auth();
