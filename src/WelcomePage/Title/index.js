3a3629e2745cebb64132801aca0af8e92c5ca8fb
MichaelEJT <ejt@ejts-mbp.wireless-1x.unc.edu>
2023-04-11 10:36:22 -0400

    buttons

--- src/WelcomePage/Title/index.js
+++ src/WelcomePage/Title/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './index.css';
+import Button from '@mui/material/Button';
 function Title() {
   return (
     <div className = "Title">
       <h1 className='Text'>UNCRoommates</h1>
-      <h2 className='Logout-text'> Logout</h2>
+      <h2 className='Logout-text'alignItems="stretch">
+        <Button style={{backgroundColor:'#13294b'}}variant='contained'>Logout</Button>
+        <Button style={{backgroundColor:'#13294b'}}variant='contained'>Settings</Button>
+      </h2>
     </div>
   )
 }
