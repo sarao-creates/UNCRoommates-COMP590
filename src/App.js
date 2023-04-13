2bff7e99ab8792d284bfbc03270cc267af420659
MichaelEJT <ejt@ejts-MBP.attlocal.net>
2023-04-13 16:12:15 -0400

    Finished the front end portion of survey

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import WelcomePage from './WelcomePage';
 import ProfilePage from './ProfilePage';
+import Survey from './Survey';
 const ProfilePath = '/profile';
 const WelcomePath = '/welcome';
+const SurveyPath = '/survey';
 
 
 
@@ -14,6 +16,7 @@ function App() {
       </Route>
       <Route path={WelcomePath} component={WelcomePage}></Route>
       <Route path={ProfilePath} component={ProfilePage}></Route>
+      <Route path={SurveyPath} component={Survey}></Route>
     </Router>
 
   );
