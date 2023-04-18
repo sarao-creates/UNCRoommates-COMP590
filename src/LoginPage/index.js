a9135598279805238100b20f069186ca18e7150f
Sameer Rao <sarao@live.unc.edu>
2023-04-18 11:02:21 -0400

    fixed some routing on signin

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -8,8 +8,11 @@ import { auth } from '../Firebase/firebase.js';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function LoginPage() {
+
+    const history = useHistory();
     
     const [email, setEmail] = useState('');
     const [pwd, setPWD] = useState('');
@@ -29,7 +32,7 @@ function LoginPage() {
     const handleSignIn = () => {
         signInWithEmailAndPassword(auth, email, pwd).then((userCredential) => {
             console.log('Sign in successful');
-            // history.push('/home');
+            history.push('/profile');
         }).catch((error) => {
             let errorCode = error.code;
             let errorMessage = error.message;
@@ -46,8 +49,8 @@ function LoginPage() {
             <div className='login-input'>
                 <h3>UNC Email:</h3> <TextField id="outlined-email-input" label="Email" type="email" onChange={handleEmail} />
                 <h3>Password:</h3> <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" onChange={handlePWD} />
-                <div><Link to='/profile'><Button variant="contained" onClick={handleSignIn}>Login</Button></Link></div>
-                <h4>Don't Have an Account? <Link to='/profile'><u>Click Here to Sign Up!</u></Link></h4>
+                <div><Button variant="contained" onClick={handleSignIn}>Login</Button></div>
+                <h4>Don't Have an Account? <Link to='/signup'><u>Click Here to Sign Up!</u></Link></h4>
             </div>
             <Snackbar open={snackbar.status} autoHideDuration={7500} onClose={() => setSnackbar({status: false})}> 
                     <Alert severity='info'>{snackbar.message}</Alert>
