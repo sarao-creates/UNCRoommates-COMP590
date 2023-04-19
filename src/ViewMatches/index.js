8a87abf12a6a5fbdb12abc5965721d7b2e3ba59e
MichaelEJT <ejt@ejts-MBP.attlocal.net>
2023-04-19 14:19:23 -0400

    set up branch for algorithm

--- src/ViewMatches/index.js
+++ src/ViewMatches/index.js
@@ -16,7 +16,7 @@ import {
 function ViewMatches() {
     const profiles = [
         {
-          id: 1,
+          order: 1,
           name: "Jingtong E",
           year: 2023,
           age: 22,
@@ -24,7 +24,7 @@ function ViewMatches() {
           bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.",
         },
         {
-          id: 2,
+          order: 2,
           name: "Sameer Rao",
           year: 2023,
           age: 22,
@@ -32,7 +32,7 @@ function ViewMatches() {
           bio: "Suspendisse imperdiet ex et varius tristique. Sed vel nisi vel nunc eleifend auctor eget eu ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.",
         },
         {
-          id: 3,
+          order: 3,
           name: "Brooke Hackney",
           year: 2023,
           age: 22,
@@ -40,7 +40,7 @@ function ViewMatches() {
           bio: "In euismod, justo quis pretium pellentesque, elit elit ullamcorper augue, eget laoreet sapien mi in velit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.",
         },
         {
-            id: 4,
+            order: 4,
             name: "John Doe",
             year: 2023,
             age: 22,
@@ -48,7 +48,7 @@ function ViewMatches() {
             bio: "Suspendisse imperdiet ex et varius tristique. Sed vel nisi vel nunc eleifend auctor eget eu ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.",
           },
           {
-            id: 5,
+            order: 5,
             name: "Jane Doe",
             year: 2023,
             age: 22,
@@ -56,7 +56,7 @@ function ViewMatches() {
             bio: "Suspendisse imperdiet ex et varius tristique. Sed vel nisi vel nunc eleifend auctor eget eu ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor ipsum vel justo maximus lacinia.",
           },
           {
-            id: 6,
+            order: 6,
             name: "Ketan Mayer-Patel",
             year: 2023,
             age: 22,
@@ -72,7 +72,7 @@ function ViewMatches() {
             <Title></Title>
             <Grid container spacing={2} sx={{ width: "66.66%", float:'right'}}>
       {profiles.map((profile) => (
-        <Grid key={profile.id} item xs={6}>
+        <Grid key={profile.order} item xs={6}>
           <Paper
             elevation={3}
             sx={{ height: "80%", padding: 2, display: "flex", flexDirection: "column", justifyContent: "space-between", cursor: "pointer" }}
