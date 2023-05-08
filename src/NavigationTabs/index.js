13e340aa3cf28b52474e49d4db3fc1a899837309
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2023-05-07 23:03:14 -0400

    small changes to navbar

--- src/NavigationTabs/index.js
+++ src/NavigationTabs/index.js
@@ -2,26 +2,21 @@ import React from 'react'
 import { NavLink } from "react-router-dom";
 import './index.css';
 
-
-
-// Cannot get the navlinks to actually go to the pages. I have researched for hours on how to do this
-// and I cannot figure it out.
-
 function NavigationTabs() {
     return (
-        <div>
-      <ul className="nav"> 
-        <li>
-          <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/profile">Account and Bio</NavLink>
-        </li>
-        <li>
-          <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/viewmatches">View Potential Matches</NavLink>
-        </li>
-        <li>
-          <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/survey">Edit Survey</NavLink>
-        </li>
-      </ul>
-    </div>
+      <div>
+        <ul className="nav"> 
+          <li>
+            <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/profile">Account and Bio</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/viewmatches">View Potential Matches</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/survey">Edit Survey</NavLink>
+          </li>
+        </ul>
+      </div>
     );
   }
   
