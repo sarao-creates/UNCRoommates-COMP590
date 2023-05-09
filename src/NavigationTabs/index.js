81b241ba534acab404111474f1e4a4a2ac135d64
Sameer Rao <sarao@live.unc.edu>
2023-05-09 09:37:31 -0400

    navbar active change

--- src/NavigationTabs/index.js
+++ src/NavigationTabs/index.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import './index.css';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../Firebase/firebase'
 
 function NavigationTabs() {
+
+    const auth = getAuth();
+    const [active, setActive] = useState(false)
+
+    useEffect(() => {
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                setActive(docSnap.data()['active'])
+            } else {
+              console.log('document does not exist')
+            }    
+
+        } else {
+          console.log('not signed in')
+        }
+
+        
+    });
+      
+      
+
+  });
+
+  if (active) {
     return (
       <div>
         <ul className="nav"> 
@@ -18,6 +50,16 @@ function NavigationTabs() {
         </ul>
       </div>
     );
-  }
-  
-  export default NavigationTabs
+  } else {
+  return (
+    <div>
+        <ul className="nav"> 
+          <li>
+            <NavLink activeStyle={{ background:'white', color: '#13294B' }} to="/profile">Account and Bio</NavLink>
+          </li>
+        </ul>
+      </div>
+  )
+}
+}
+  export default NavigationTabs;
